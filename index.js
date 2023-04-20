9f9f43fefdf9f078f2ea193ee7aef86feb4dd25b
cijjas <cijjas@itba.edu.ar>
2023-04-20 09:56:03 -0300

    4000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/', (req, res) => res.send('Hello World!'));   // http://localhost:3000/
 
